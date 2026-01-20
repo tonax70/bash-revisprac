aec2b8a8816858aa84d53e3c102ba785beb41168
Toantoanhacker <canhtoan9262022@gmail.com>
2026-01-20 15:25:40 +0700

    modify ls info add common flags

--- a_ls.f90
+++ a_ls.f90
@@ -2,4 +2,11 @@ program run_ls
   implicit none;
   print *, "This is when you run the ls command:";
   call execute_command_line("ls");
+  print *, "Flags:";
+  print *, "-l : long list format";
+  call execute_command_line("ls -l");
+  print *, "-a : include hidden files";
+  call execute_command_line("ls -a");
+  print *, "-R : list sub-directory recursively";
+  call execute_command_line("ls -R");
 end program run_ls
