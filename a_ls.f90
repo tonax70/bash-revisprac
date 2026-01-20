1f5fa0456e80dfa528e9582f14aa2b737d4c105d
Toantoanhacker <canhtoan9262022@gmail.com>
2026-01-20 15:08:45 +0700

    the cd command

--- a_ls.f90
+++ a_ls.f90
@@ -1,5 +1,5 @@
 program run_ls
-  implicit none
-
-  call execute_command_line("ls")
+  implicit none;
+  print *, "This is when you run the ls command:";
+  call execute_command_line("ls");
 end program run_ls
---
+++ b_cd.f90
@@ -0,0 +1,6 @@
+program c_cd
+    implicit none;
+    print *, "- The cd command uses to move in or out a directory";
+    print *, "- cd frequently uses with the ls command to navigate across the file tree";
+    print *, "- we can use cd .. to move out directory";
+end program c_cd
