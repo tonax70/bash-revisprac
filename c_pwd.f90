c63975201993fed981c10d6ab08bf48fed8bfc90
Toantoanhacker <canhtoan9262022@gmail.com>
2026-01-20 15:13:20 +0700

    the pwd command

---
+++ c_pwd.f90
@@ -0,0 +1,6 @@
+program printWorkingDir;
+    print *, "pwd stands for: Print Working Directory";
+    print *, "It will simply print working directory as text to the terminal";
+    print *, "So we can know exactly which directory we are currently in";
+    call execute_command_line("pwd");
+end program printWorkingDir;
