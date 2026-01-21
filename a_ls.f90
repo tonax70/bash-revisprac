479cfdabf8ca0c43c964f8f8a10432bb89893ec2
Toantoanhacker <canhtoan9262022@gmail.com>
2026-01-21 14:49:47 +0700

    updated section a

--- a_ls.f90
+++ a_ls.f90
@@ -3,12 +3,32 @@ program run_ls
   print *, "This is when you run the ls command:";
   call execute_command_line("ls");
   print *, "Flags:";
-  print *, "-l : long list format";
-  call execute_command_line("ls -l");
-  print *, "-a : include hidden files";
-  call execute_command_line("ls -a");
-  print *, "-R : list sub-directory recursively";
-  call execute_command_line("ls -R");
+  call ls("");
+  call ls("-l");
+  call ls("-a");
+  call ls("-R");
 
-  
+  contains
+  subroutine ls(flag)
+    implicit none;
+    character (len=*), intent(in) :: flag;
+
+    select case (trim(flag))
+    case ('')
+      print *, "This is when you run the ls command:";
+      call execute_command_line("ls");
+    case ('-l')
+      print *, "-l : long list format";
+      call execute_command_line("ls -l");
+    case ('-a')
+      print *, "-a : include hidden files";
+      call execute_command_line("ls -a");
+    case ('-R')
+      print *, "-R : list sub-directory recursively";
+      call execute_command_line("ls -R");
+    case default
+      print *, "Invalid flag, if you want no flag just input an empty string";
+    end select;
+end subroutine ls;
+
 end program run_ls
