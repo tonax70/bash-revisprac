7d0ad36e3b3e4cdfcacf7c3ff48183b67fd28e45
Toantoanhacker <canhtoan9262022@gmail.com>
2026-01-23 09:35:15 +0700

    rest mon command

---
+++ system-monitoring/dot_dot_rest.f95
@@ -0,0 +1,12 @@
+program rest 
+    implicit none;
+    print *, "There are more commands to monitoring the system: "
+    print *, "Bash top Command - Display Linux Tasks";
+    print *, "Bash df Command - File System Disk Space Usage";
+    print *, "Bash du Command - File Space Usage";
+    print *, "Bash free Command - Display Free and Used Memory";
+    print *, "Bash kill Command - Terminate Processes";
+    print *, "Bash uptime Command - System Runtime";
+    print *, "Unfortunately we are using git bash or windows so some aren't available right now, but we already reviewed that";
+    print *, "ANd might be later on a linux pc";
+end program rest
